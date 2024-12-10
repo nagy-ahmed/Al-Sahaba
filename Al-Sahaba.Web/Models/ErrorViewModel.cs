940334bd7ebda0bdb33e09d4ae40c5103ec1704c
Ahmed Nagy <ahmednagydawood@gmail.com>
2024-12-11 01:15:51 +0200

    Add project files.

---
+++ Al-Sahaba.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Al_Sahaba.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
