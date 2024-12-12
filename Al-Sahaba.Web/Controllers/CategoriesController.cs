ff7775f2e05bd023b5811957b5646a7a0725c193
Ahmed Nagy <ahmednagydawood@gmail.com>
2024-12-12 22:42:11 +0200

    make categories index

---
+++ Al-Sahaba.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Al_Sahaba.Web.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CategoriesController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			//todo : use viewModel
+			var categories = _context.Categories.ToList();
+			return View(categories);
+		}
+	}
+}
